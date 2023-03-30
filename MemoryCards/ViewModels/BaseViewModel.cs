6b23ae3950ee16dec7ea45ea98c7e9ae8e4bd410
simkaa47 <simkaa7@gmail.com>
2023-03-30 22:02:58 +0300

    BaseViewModel was created

---
+++ MemoryCards/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MemoryCards.ViewModels
+{
+    public partial class BaseViewModel:ObservableObject
+    {
+        public BaseViewModel()
+        {
+
+        }
+        [ObservableProperty]
+        string _title;
+        [ObservableProperty]
+        string _isBusy;
+    }
+}
