9f45a63933e1b40d2d7e3f2a002cccb47a07c5c1
Serafim Prilezhaev <simkaa7@gmail.com>
2023-05-01 21:45:20 +0300

    ApplicationContext class (based from DbContext) was created

---
+++ MemoryCards/DataAccess/ApplicationContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MemoryCards.DataAccess
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityCommon>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.Entity.LastChangedTime = DateTime.Now;
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreatedTime = DateTime.Now;
+                        break;
+                }
+            }
+            return await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
