64e92a2cac80c6b81c347d1cc376ac340e57083f
Serafim Prilezhaev <simkaa7@gmail.com>
2023-05-09 01:58:50 +0300

    First db migration was added

---
+++ MemoryCards.DbMigrator/Program.cs
@@ -0,0 +1,17 @@
+﻿using MemoryCards.Persistance.Services;
+
+namespace MemoryCards.DbMigrator
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Migrator running..");
+
+            using (var context = new ApplicationContext())
+            {
+                var all = context.GameCommonSettingses.ToList();
+            }
+        }
+    }
+}
---
+++ MemoryCards.Persistance/Migrations/20230508223457_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemoryCards.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameCommonSettingses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GameType = table.Column<int>(type: "INTEGER", nullable: false),
+                    AvialableLevel = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastChangedTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameCommonSettingses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameCommonSettingses");
+        }
+    }
+}
