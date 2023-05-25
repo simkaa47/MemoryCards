5d038a0fc93062d6eb362bb06267b4c5933f0940
Serafim Prilezhaev <simkaa7@gmail.com>
2023-05-26 00:35:01 +0300

    Add divideconverter

---
+++ MemoryCards/Infrastructure/Converters/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace MemoryCards.Infrastructure.Converters
+{
+    public class Converter : IValueConverter
+    {
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
